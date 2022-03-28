2542333d85e45cdc24d7fb64a602f19748cdefd9
Shivani Gupta <shivani.gupta@headstrait.com>
2022-03-29 02:30:10 +0530

    make it better

--- server/src/app.js
+++ server/src/app.js
@@ -39,6 +39,6 @@ if (process.env.NODE_ENV == "production") {
   //static folder
   app.use(express.static(__dirname + "/public"));
   // handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "../public/index.html"));
 }
 module.exports = app;
